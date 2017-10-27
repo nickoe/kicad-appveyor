6ff51dda1d3272c8d5987f19568d68cca2f02317
Nick Østergaard <oe.nick@gmail.com>
2017-10-27 22:03:25 +0200

    zliv

---
+++ build_zlib.bat
@@ -0,0 +1,8 @@
+call "C:\Program Files\Microsoft SDKs\Windows\v7.1\Bin\SetEnv.cmd" /x64
+call "C:\Program Files (x86)\Microsoft Visual Studio 14.0\VC\vcvarsall.bat" x86_amd64
+ 
+:: zlib
+
+git clone -b v1.2.8 https://github.com/madler/zlib.git
+call cd zlib && nmake /f win32\Makefile.msc && cd ..
+call cp zlib.lib,zlib1.dll,zlib1.pdb,zdll.lib,zlib.h,zconf.h cache/

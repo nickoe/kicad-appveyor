07476411d5d5069034fef82901ee73a96a0229ea
Nick Østergaard <oe.nick@gmail.com>
2017-10-28 13:28:28 +0200

    ds:

--- build_zlib.bat
+++ build_zlib.bat
@@ -1,6 +1,3 @@
-call "C:\Program Files\Microsoft SDKs\Windows\v7.1\Bin\SetEnv.cmd" /x64
-call "C:\Program Files (x86)\Microsoft Visual Studio 14.0\VC\vcvarsall.bat" x86_amd64
- 
 :: zlib
 
 git clone -b v1.2.8 https://github.com/madler/zlib.git
@@ -8,7 +5,7 @@ call cd zlib && nmake /f win32\Makefile.msc
 call dir .
 ::call cp zlib.lib,zlib1.dll,zlib1.pdb,zdll.lib,zlib.h,zconf.h cache/
 ::call xcopy /I zlib.lib zlib1.dll zlib1.pdb zdll.lib zlib.h zconf.h cache
-mkdir cache
+::mkdir cache
 for %%i in (zlib.lib,zlib1.dll,zlib1.pdb,zdll.lib,zlib.h,zconf.h) do (
   echo %%i
   xcopy %%i cache 

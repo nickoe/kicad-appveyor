3bec8fda65dab7a8c60678234f3d5784805f7739
Nick Østergaard <oe.nick@gmail.com>
2017-10-28 13:27:19 +0200

    sdf

--- build_zlib.bat
+++ build_zlib.bat
@@ -11,7 +11,7 @@ call dir .
 mkdir cache
 for %%i in (zlib.lib,zlib1.dll,zlib1.pdb,zdll.lib,zlib.h,zconf.h) do (
   echo %%i
-  xcopy %%i cache /E /I
+  xcopy %%i cache 
 )
 dir cache
 cd %WORKSPACE%

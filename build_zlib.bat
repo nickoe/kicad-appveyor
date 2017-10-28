9e54207fab3955c26631d065ee843d0bb7d8b518
Nick Østergaard <oe.nick@gmail.com>
2017-10-28 13:14:52 +0200

    or

--- build_zlib.bat
+++ build_zlib.bat
@@ -7,6 +7,8 @@ git clone -b v1.2.8 https://github.com/madler/zlib.git
 call cd zlib && nmake /f win32\Makefile.msc
 call dir .
 ::call cp zlib.lib,zlib1.dll,zlib1.pdb,zdll.lib,zlib.h,zconf.h cache/
-call xcopy /I zlib.lib zlib1.dll zlib1.pdb zdll.lib zlib.h zconf.h cache
+::call xcopy /I zlib.lib zlib1.dll zlib1.pdb zdll.lib zlib.h zconf.h cache
+for %%i in (zlib.lib,zlib1.dll,zlib1.pdb,zdll.lib,zlib.h,zconf.h) do
+  xcopy %%i cache /I
 dir cache
 cd %WORKSPACE%

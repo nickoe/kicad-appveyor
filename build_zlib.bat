a996a51ae6f5ed8790189dfa41bdb0b879bb6c7a
Nick Østergaard <oe.nick@gmail.com>
2017-10-28 13:47:20 +0200

    zlib

--- build_zlib.bat
+++ build_zlib.bat
@@ -5,10 +5,10 @@ call cd zlib && nmake /f win32\Makefile.msc
 call dir .
 ::call cp zlib.lib,zlib1.dll,zlib1.pdb,zdll.lib,zlib.h,zconf.h cache/
 ::call xcopy /I zlib.lib zlib1.dll zlib1.pdb zdll.lib zlib.h zconf.h cache
-::mkdir cache
+mkdir cache/zlib
 for %%i in (zlib.lib,zlib1.dll,zlib1.pdb,zdll.lib,zlib.h,zconf.h) do (
   echo %%i
-  xcopy %%i cache /I
+  xcopy %%i cache/zlib /I
 )
-dir cache
+dir cache/zlib
 cd %WORKSPACE%
